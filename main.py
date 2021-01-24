e069621357921c98fff0b137a7223b7d1d95bd84
lau <lau.bannenberg@gmail.com>
2021-01-24 11:48:14 +0100

    add basic sklearn functionality

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+import pandas as pd
+
+
 def hello_world():
     return "Hello!"
+
+
+def get_iris(test_size=0.4):
+    X, y = load_iris(as_frame=True, return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    return X, y, X_train, X_test, y_train, y_test
+
+
+if __name__ == '__main__':
+    pass
--- main_test.py
+++ main_test.py
@@ -1,4 +1,5 @@
 import main
+import pandas as pd
 
 
 def test_hello_world():
@@ -7,3 +8,26 @@ def test_hello_world():
 
 def test_hello_world_again():
     assert main.hello_world() == 'Hello World!'
+
+
+def test_iris_split():
+    X, y, X_train, X_test, y_train, y_test = main.get_iris(test_size=0.4)
+    assert isinstance(X, pd.DataFrame)
+    assert isinstance(y, pd.Series)
+    assert isinstance(X_train, pd.DataFrame)
+    assert isinstance(X_test, pd.DataFrame)
+    assert isinstance(y_train, pd.Series)
+    assert isinstance(y_test, pd.Series)
+    assert X.shape == (150, 4)
+    assert y.shape == (150,)
+    assert X_train.shape == (90, 4)
+    assert X_test.shape == (60, 4)
+    assert y_train.shape == (90,)
+    assert y_test.shape == (60,)
+    X, y, X_train, X_test, y_train, y_test = main.get_iris(test_size=0.6)
+    assert X.shape == (150, 4)
+    assert y.shape == (150,)
+    assert X_train.shape == (60, 4)
+    assert X_test.shape == (90, 4)
+    assert y_train.shape == (60,)
+    assert y_test.shape == (90,)
